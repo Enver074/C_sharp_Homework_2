5865b7530cb55849bc20a36d8566596bc94fc722
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2022-10-06 00:03:49 +0300

    Третье задание

--- Program.cs
+++ Program.cs
@@ -26,26 +26,53 @@
 // 32679 -> 6
 
 
-Console.WriteLine("------Второе задание ------");
-Console.WriteLine("Введите число");
-int number = int.Parse(Console.ReadLine()!);
-if ((number>=100) && (number<1000)){
-    int num1 = number%100;
-    int num2 = num1/10;
-    Console.WriteLine($"Вторая цифра числа {number} - {num2}"); } 
-if ((number>=1000) && (number<10000)){
-    int num1 = number%1000;
-    int num2 = num1/100;
-    Console.WriteLine($"Вторая цифра числа {number} - {num2}"); }
-if ((number>=10000) && (number<100000)){
-    int num1 = number%10000;
-    int num2 = num1/1000;
-    Console.WriteLine($"Вторая цифра числа {number} - {num2}"); }    
-if (number<100)
-    {
-    Console.WriteLine("Нет третьей цифры"); 
+// Console.WriteLine("------Второе задание ------");
+// Console.WriteLine("Введите число");
+// int number = int.Parse(Console.ReadLine()!);
+// if ((number>=100) && (number<1000)){
+//     int num1 = number%100;
+//     int num2 = num1/10;
+//     Console.WriteLine($"Вторая цифра числа {number} - {num2}"); } 
+// if ((number>=1000) && (number<10000)){
+//     int num1 = number%1000;
+//     int num2 = num1/100;
+//     Console.WriteLine($"Вторая цифра числа {number} - {num2}"); }
+// if ((number>=10000) && (number<100000)){
+//     int num1 = number%10000;
+//     int num2 = num1/1000;
+//     Console.WriteLine($"Вторая цифра числа {number} - {num2}"); }    
+// if (number<100)
+//     {
+//     Console.WriteLine("Нет третьей цифры"); 
+//     }
+// else
+//     {
+//     Console.WriteLine("Число больше 100000"); 
+//     }
+
+
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным. Обязательно сделать проверку на ввод чисел меньше 1 и больше 7
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("------Третье задание ------");
+Console.WriteLine("Введите цифру");
+int number = int.Parse(Console.ReadLine()!);{
+if ((number>=1) && (number<=7))
+    { 
+    if (number == 6 || number == 7)
+        {
+        Console.WriteLine("Выходной");
+        }
+    else 
+        {
+        Console.WriteLine("Будни");
+        }
     }
 else
-    {
-    Console.WriteLine("Число больше 100000"); 
-    }
+    Console.WriteLine("Введите цифру от 1 до 7");
+}
